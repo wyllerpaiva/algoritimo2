32dcd41d646d69a48e5353aed4fd3e78eb5ae5e3
wylller paiva <willerpaiva10@gmail.com>
2016-10-19 21:58:09 -0200

    projeto driver.

---
+++ Driver/src/MainApp.java
@@ -0,0 +1,14 @@
+
+public class MainApp {
+	public static void main(String[] args) {
+		int pontosNaCarteira = 22;
+		if (pontosNaCarteira < 20) {
+			System.out.println("Pode dirigir");
+		} else {
+			System.out.println("Não pde dirigir");
+			System.out.println("Seu Barbeiro");
+		}
+		
+	}
+
+}
---
+++ Driver/src/idade.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class idade {
+	public static void main(String[] args) {
+//		Scanner milk = new Scanner(System.in);
+//		System.out.println("Quantos pontos você tem carteira?");
+//		int pontosNaCarteira = milk.nextInt();
+//		System.out.println("Qual a sua idade ?");
+//		int idade = milk.nextInt();
+//		if (pontosNaCarteira <= 20 && (idade >= 18)) {
+//
+//			System.out.println("Pode Passar ");
+//		} else {
+//			System.out.println("Não Pode Passar");
+//			milk.close();
+//		}
+//	}
+//}
+
+	int a =2;
+	int b =3;
+	
+	System.out.println(a<-1);
+	
+	}
+}
